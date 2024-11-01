9989ec0cd6dd5be5caa9a743382de852508eca0b
BangLK <bangle69.work@gmail.com>
2024-11-02 05:24:13 +0700

    complete project

---
+++ BussinessObject/DataContext/BussinessContext.cs
@@ -0,0 +1,29 @@
+﻿using BussinessObject.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace BussinessObject.DataContext
+{
+    public class BussinessContext : DbContext
+    {
+        public BussinessContext() { }
+
+        public BussinessContext(DbContextOptions<BussinessContext> options) : base(options) { }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("BangConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
---
+++ MVC/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BussinessObject.DataContext;
+using BussinessObject.Model;
+
+namespace MVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        
+
+        public StudentsController()
+        {
+
+        }
+
+        // GET: Students
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            return View();
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {  
+            return View();
+        }
+    }
+}
